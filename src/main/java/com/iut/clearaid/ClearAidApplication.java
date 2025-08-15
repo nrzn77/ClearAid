b23e121d0c3bff0420c9aaa77d7927dd45839732
areeb <areeb@gmail.com>
2025-08-15 10:04:47 +0600

    Initialized project

---
+++ src/main/java/com/iut/clearaid/ClearAidApplication.java
@@ -0,0 +1,13 @@
+package com.iut.clearaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearAidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearAidApplication.class, args);
+    }
+
+}
